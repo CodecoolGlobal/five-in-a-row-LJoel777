494636f34909372e11fbbc2cefb189aa2a82f63a
Adam Elek <elekadam21@gmail.com>
2020-04-16 14:09:44 +0200

    ai_decision

--- src/main/java/com/codecool/fiveinarow/AiDecision.java
+++ src/main/java/com/codecool/fiveinarow/AiDecision.java
@@ -27,7 +27,6 @@ public class AiDecision {
             return coords;
         }
         return null;
-        //return winChecker(board);
     }
 
     private int[] getCoords(int[][] decisionArray) {
@@ -39,10 +38,21 @@ public class AiDecision {
         return null;
     }
 
-    /*public boolean verticalCheck() {
-        int[][] transposedBoard = matrixTranspose(board);
-        return winChecker(transposedBoard);
-    }*/
+    public int[] verticalCheck() {
+        int[][][] transposedBoardWithCoords = new int[board[0].length][board.length][3];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                transposedBoardWithCoords[j][i] = new int[]{board[i][j], i, j};
+            }
+        }
+        int[][] decisionArray = decisionChecker(transposedBoardWithCoords, new int[]{1, 1, 1, 1, 0});
+        if (decisionArray != null) {
+            int[] coords = getCoords(decisionArray);
+            System.out.println(Arrays.toString(coords));
+            return coords;
+        }
+        return null;
+    }
 
     private int[][] decisionChecker(int[][][] newBoard, int[] arrayToCheck) {
         for (int[][] row : newBoard) {
@@ -54,7 +64,7 @@ public class AiDecision {
                     decisionArray[j] = row[i + j][0];
                 }
                 if (Arrays.equals(decisionArray, arrayToCheck)) {
-                    System.out.println(decisionArrayWithCoords);
+                    System.out.println(Arrays.deepToString(decisionArrayWithCoords));
                     return decisionArrayWithCoords;
                 }
             }
@@ -62,42 +72,33 @@ public class AiDecision {
         return null;
     }
 
-    public int[][] matrixTranspose(int[][] oldBoard) {
-        int[][] transposedBoard = new int[oldBoard[0].length][oldBoard.length];
-        for (int i = 0; i < oldBoard.length; i++) {
-            for (int j = 0; j < oldBoard[0].length; j++) {
-                transposedBoard[j][i] = oldBoard[i][j];
-            }
-        }
-        return transposedBoard;
-    }
 
-    /*public boolean hasWonDiagonally() {
-        boolean[] booleanListForDiagonals = new boolean[board.length - (howMany - 1)];
+    public int[] diagonalCheck() {
+        //boolean[] booleanListForDiagonals = new boolean[board.length - (howMany - 1)];
         for (int i = (howMany - 1); i < board.length; i++) {
-            int[][] boardOfDiagonals = new int[4][i + 1];
-            int[] rowTopLeft = new int[i + 1];
-            int[] rowTopRight = new int[i + 1];
-            int[] rowBottomLeft = new int[i + 1];
-            int[] rowBottomRight = new int[i + 1];
+            int[][][] boardOfDiagonals = new int[4][i + 1][3];
+            int[][] rowTopLeft = new int[i + 1][3];
+            int[][] rowTopRight = new int[i + 1][3];
+            int[][] rowBottomLeft = new int[i + 1][3];
+            int[][] rowBottomRight = new int[i + 1][3];
 
             for (int j = 0; j < i + 1; j++) {
-                rowTopLeft[j] = board[i - j][j];
-                rowTopRight[j] = board[j][board.length - 1 - (i - j)];
-                rowBottomLeft[j] = board[board.length - 1 - (i - j)][j];
-                rowBottomRight[j] = board[board.length - 1 - j][board.length - 1 - (i - j)];
+                rowTopLeft[j] = new int[]{board[i - j][j], i - j, j};
+                rowTopRight[j] = new int[]{board[j][board.length - 1 - (i - j)], j, board.length - 1 - (i - j)};
+                rowBottomLeft[j] = new int[]{board[board.length - 1 - j][i - j], board.length - 1 - j, i - j};
+                rowBottomRight[j] = new int[]{board[board.length - 1 - (i - j)][board.length - 1 - j], board.length - 1 - (i - j), board.length - 1 - j};
             }
             boardOfDiagonals[0] = rowTopLeft;
             boardOfDiagonals[1] = rowTopRight;
             boardOfDiagonals[2] = rowBottomLeft;
             boardOfDiagonals[3] = rowBottomRight;
-            booleanListForDiagonals[i - (howMany - 1)] = winChecker(boardOfDiagonals);
-        }
-        for (boolean element : booleanListForDiagonals) {
-            if (element) {
-                return true;
+            int[][] decisionArray = decisionChecker(boardOfDiagonals, new int[]{1, 1, 1, 1, 0});
+            if (decisionArray != null) {
+                int[] coords = getCoords(decisionArray);
+                System.out.println(Arrays.toString(coords));
+                return coords;
             }
         }
-        return false;
-    }*/
+        return null;
+    }
 }
